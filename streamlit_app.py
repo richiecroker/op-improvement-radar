74dd2a3dd845e348ec76f24b0c5e51a18507e613
Richard Croker <ra.croker@gmail.com>
2026-03-30 16:22:57 +0100

    fix project name

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,7 +74,7 @@ st.dataframe(measures_df)
 
 PREFIXES = ["ccg", "pcn", "stp"]
 
-sample_table = bq.get_table(f"{PROJECT}.{DATASET}.ccg_data_lpzomnibus")
+sample_table = bq.get_table(f"ebmdatalab.measures.ccg_data_lpzomnibus")
 col_list = ", ".join(
     f.name for f in sample_table.schema
     if f.name not in {"stp_id", "regional_team_id"}
