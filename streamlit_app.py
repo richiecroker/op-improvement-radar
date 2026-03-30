ad344a9c4c806d17c1777900c73c5553a671e352
Richard Croker <ra.croker@gmail.com>
2026-03-30 14:52:52 +0100

    remove svg for now

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ st.set_page_config(layout="wide")
 
 # --- Header ---
 base_dir = os.path.dirname(__file__)
-st.image(os.path.join(base_dir, "content", "OpenPrescribing.svg"))
+#st.image(os.path.join(base_dir, "content", "OpenPrescribing.svg"))
 st.info(
     """##### Hello!  This is a **very** early prototype of something.  
 Please let us know what you think, and what you'd like to see.  Email us at [bennett@phc.ox.ac.uk](mailto:bennett@phc.ox.ac.uk)"""
@@ -94,14 +94,14 @@ st.dataframe(df)
 
 st.divider()
 
-with st.expander("Click here to read our methodology", icon=":material/quick_reference:"):
-    with open(os.path.join(base_dir, "content", "methodology.md")) as f:
-        st.markdown(f.read())
+#with st.expander("Click here to read our methodology", icon=":material/quick_reference:"):
+#    with open(os.path.join(base_dir, "content", "methodology.md")) as f:
+#        st.markdown(f.read())
 
-with open(os.path.join(base_dir, "content", "changelog.yaml")) as f:
-    changelog = yaml.safe_load(f)
+#with open(os.path.join(base_dir, "content", "changelog.yaml")) as f:
+#    changelog = yaml.safe_load(f)
 
-with st.expander("Click to see changelog", icon=":material/history:"):
-    for entry in reversed(changelog):
-        st.markdown(f"**{entry['date']}** — {entry['change']} *({entry['person']})*")
+#with st.expander("Click to see changelog", icon=":material/history:"):
+ #   for entry in reversed(changelog):
+ #       st.markdown(f"**{entry['date']}** — {entry['change']} *({entry['person']})*")
 
