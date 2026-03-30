603bd1fb219d02930f3b9ed75a76cfc2cf4e353c
Richard Croker <ra.croker@gmail.com>
2026-03-30 21:52:19 +0100

    correct column

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,9 +61,9 @@ df = conn.execute(
     agg AS (
         SELECT
             org_id,
-            AVG(numerator)                                             AS mean_events,
-            AVG(CASE WHEN rn_asc  <= 6 THEN rate       END)           AS start_rate,
-            AVG(CASE WHEN rn_desc <= 6 THEN rate       END)           AS end_rate,
+            AVG(numerator)                                            AS mean_events,
+            AVG(CASE WHEN rn_asc  <= 6 THEN cal_value  END)           AS start_rate,
+            AVG(CASE WHEN rn_desc <= 6 THEN cal_value  END)           AS end_rate,
             AVG(CASE WHEN rn_asc  <= 6 THEN percentile END)           AS start_pct,
             AVG(CASE WHEN rn_desc <= 6 THEN percentile END)           AS end_pct
         FROM ranked
